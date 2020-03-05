69c79266324a48fd8de788484ece432d29cc4c9c
vvvmvvv <boxingvm16@gmail.com>
2020-03-05 15:09:38 +0200

    add starter package assets

--- fdband/src/components/Main/Main.js
+++ fdband/src/components/Main/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Main.scss';
 
+import panda from '../../assets/images/panda.jpg'
+
 const Main = () => {
     return (
         <React.Fragment>
             <h1>Главная страница</h1>
+            <img src={panda} alt="panda" />
             
         </React.Fragment>
     );
