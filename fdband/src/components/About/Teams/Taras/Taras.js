3c710c942fdaada57e91257b30134a10bfcc339e
Vladimir <boxingvm16@gmail.com>
2020-03-12 23:53:52 +0200

    about comp v3

--- fdband/src/components/About/Teams/Taras/Taras.js
+++ fdband/src/components/About/Teams/Taras/Taras.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+
 import './Taras.scss';
 import '../TeamCss/aboutme.css';
 import '../TeamCss/animations.css';
@@ -7,40 +8,28 @@ import '../TeamCss/social.css';
 import '../TeamCss/style.css';
 import '../TeamCss/backToTeam.css';
 
-
 import tarasPng from './imgTaras/taras.png'
 import imgGit from "../../../../assets/images/iconsLena/git.png"
 import imgLinkedin from "../../../../assets/images/iconsLena/linkedin.png";
-import about2 from '../../../../assets/images/about2.JPG'
-import aboutme_photo1 from '../../../../assets/images/aboutme_photo1.JPG'
-import door from '../../../../assets/images/door.JPG'
+import taras2 from '../../../../assets/images/g2.jpeg'
 
 const Taras = () => {
     return (
         <React.Fragment>
             <div className="team-inner">
              <div className="team_header">
-            <Link  to="/aboutUs#ourBand"><div class="backToTeam"><p align="right">Back to team</p></div></Link>
-            
-                <div className="team_header__bg">
-                
-                {/* <h2>Page by Lena</h2> */}
+            <Link  to="/aboutUs#ourBand"><div class="backToTeam"><p align="right">Back to team</p></div></Link>       
+                <div className="team_header__bg">=
                     <ul className="team_header-nav">
                         <li className="team_header-nav__item"><a href="#about-me">About me</a></li>
-                        {/* <li className="team_header-nav__item"><a href="#gallery">Our team</a></li> */}
                         <li className="team_header-nav__item"><a href="#social">Contact me</a></li>
                     </ul>
                 </div>
-
                 <div className="team_wrapper">
-                
                     <div className="team_header__container team_container">
-
-
                         <img className="taras_header-banner__photo" src={tarasPng} alt="banner" />
-
                         <div className="team_header__text text">
-                            <h1 className="team_header-text__title title">Lena Vu</h1>
+                            <h1 className="team_header-text__title title">Taras Moskalenko</h1>
                             <p className="team_header-text__description description">Welcome to my page! Get to know me a bit better!</p>
                             <p className="team_header-text__description description">My life has changed overnigth since I joined the band!</p>
                             <p className="team_header-text__description description"> So Happy to be a part of this team!</p>
@@ -48,42 +37,31 @@ const Taras = () => {
                     </div>
                 </div>
             </div>
-
             <div className="about-me" id="about-me">
-            
                 <div className="team_wrapper">
                     <div className="about-me__container1 team_container">
                         <div className="about-me__text text">
                             <h1 className="about-me-text__title title">About me</h1>
-                            <p className="about-me-text__description description">I am from Vietnam. Apart from music I am also keen on art and taking beautiful photos</p>
+                            <p className="about-me-text__description description">I am from Sumy. I like listen to music and write own songs.</p>
                         </div>
-                        <img className="about-me__photo photo " src={aboutme_photo1} />
                     </div>
-
                     <div className="about-me__container2 team_container">
-                        <img className="about-me__photo photo" src={about2} />
                         <div className="about-me__text text">
                             <h1 className="about-me-text__title title">My role </h1>
-                            <p className="about-me-text__description description">I play the guitar and sing</p>
+                            <p className="about-me-text__description description">I play the acoustic guitar.</p>
                         </div>
-                        <img className="about-me__photo3 photo" src={door} />
+                        <img className="about-me__photo3 photo" src={taras2} />
 
                     </div>
 
                 </div>
             </div>
-
-
-
-
             <div className="social" id="social">
                 <div className="team_wrapper">
                     <div className="h1__wrapper">
                         <h1 className="title">Contact Me</h1>
                     </div>
                     <ul className="social-media__list social-transition">
-
-
                         <li className="social-media__item git"><a href='https://github.com/haodangthi' target="_blank"><img src={imgGit} alt="git" /></a></li>
                         <li className="social-media__item linkedin"><a href="https://www.linkedin.com/in/helen-vu-7094b218b/" target="_blank"><img src={imgLinkedin} alt="linkedin" /></a></li>
                     </ul>
